1270f9161c2652c30839339e9171017730c62059
Noah Ogrydziak <12644755+notarobot767@users.noreply.github.com>
2021-07-28 20:16:31 -0400

    working guac

--- app/guac_db/schema/001-create-msql-user.sql
+++ app/guac_db/schema/001-create-msql-user.sql
@@ -1,6 +1,4 @@
 CREATE DATABASE guac;
-CREATE USER 'guacadmin'@'localhost' identified by 'guacadmin';
 CREATE USER 'guacadmin'@'%' identified by 'guacadmin';
-GRANT ALL ON guac.* TO guacadmin@'localhost';
-GRANT ALL ON guac.* TO guacadmin@'%';
+GRANT SELECT,INSERT,UPDATE,DELETE ON guac.* TO 'guacadmin'@'%';
 flush privileges;
