5896a2851ce83d577cbac023025643b820eb1277
Noah Ogrydziak <12644755+notarobot767@users.noreply.github.com>
2021-08-23 15:19:43 -0400

    updated devbox

--- app/freeradius/radius_db/docker-entrypoint-initdb.d/01_create_db_add_user.sql
+++ app/freeradius/radius_db/docker-entrypoint-initdb.d/01_create_db_add_user.sql
@@ -1,6 +1,4 @@
 CREATE DATABASE radius;
-CREATE USER 'radiusadmin'@'localhost' identified by 'cisco';
 CREATE USER 'radiusadmin'@'%' identified by 'cisco';
-GRANT ALL ON radius.* TO radiusadmin@'localhost';
 GRANT ALL ON radius.* TO radiusadmin@'%';
 flush privileges;
