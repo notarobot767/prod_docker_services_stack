02a66058e21d61e95cf05cffadb3e0a1381e498e
Noah Ogrydziak <12644755+notarobot767@users.noreply.github.com>
2021-09-02 00:38:18 -0400

    added .init file to devbox

---
+++ app/devbox/init.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if [ -z "$USER" ]
+#if var USER nonempty
+
+then
+  echo "USER enviornment variable unset!"
+else
+  useradd -m -s /bin/bash $USER
+  usermod -aG sudo $USER
+  passwd -d $USER
+
+  echo "AllowUsers $USER" >> /etc/ssh/sshd_config
+  cd /home/$USER
+  mkdir .ssh
+  ssh-keygen -q -t rsa -b 2048 -N '' \
+    -f /home/$USER/.ssh/id_rsa \
+    -C $USER@$HOSTNAME
+  chmod 400 .ssh/id_rsa
+  chmod 444 .ssh/id_rsa.pub
+  #create user, add to sudo group, remove password
+  
+  if [ -z "$SSH_KEY" ]
+    #if var SSH_KEY nonempty
+  then
+    echo "SSH_KEY enviornment variable unset!"
+  else
+    echo $SSH_KEY > .ssh/authorized_keys
+    #create auth key file for pub key login
+  fi
+
+  chown -R $USER:$USER /home/$USER
+  #ensure all ssh files are owned by USER
+fi
+
+/usr/sbin/sshd -D
+#start ssh service in background
