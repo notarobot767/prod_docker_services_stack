df42649ccc7c94df8e55b33e5f7914a416bdd21d
notarobot767 <12644755+notarobot767@users.noreply.github.com>
2022-07-03 22:58:08 -1000

    zabbix and devbox

--- app/devbox/init.sh
+++ app/devbox/init.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
-if [ -z "$USER" ]
+if [ -z "$DEV_USER" ]
 #if var USER empty
 then
   echo "USER enviornment variable unset!"
 else
-  useradd -m -s /bin/bash $USER
-  usermod -aG sudo $USER
-  passwd -d $USER
+  useradd -m -s /bin/bash $DEV_USER
+  usermod -aG sudo $DEV_USER
+  passwd -d $DEV_USER
 
-  echo "AllowUsers $USER" >> /etc/ssh/sshd_config
-  cd /home/$USER
+  echo "AllowUsers $DEV_USER" >> /etc/ssh/sshd_config
+  cd /home/$DEV_USER
   mkdir .ssh
   ssh-keygen -q -t rsa -b 2048 -N '' \
-    -f /home/$USER/.ssh/id_rsa \
-    -C $USER@$HOSTNAME
+    -f /home/$DEV_USER/.ssh/id_rsa \
+    -C $DEV_USER@$HOSTNAME
   chmod 400 .ssh/id_rsa
   chmod 444 .ssh/id_rsa.pub
   #create user, add to sudo group, remove password
   
-  if [ -z "$SSH_KEY" ]
+  if [ -z "$DEV_SSH_KEY" ]
     #if var SSH_KEY empty
   then
     echo "SSH_KEY enviornment variable unset!"
   else
-    echo $SSH_KEY > .ssh/authorized_keys
+    echo $DEV_SSH_KEY > .ssh/authorized_keys
     #create auth key file for pub key login
   fi
 
-  chown -R $USER:$USER /home/$USER
+  chown -R $DEV_USER:$USER /home/$DEV_USER
   #ensure all ssh files are owned by USER
 fi
 
