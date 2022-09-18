a24d1a3e37e0667a0445b32d7f286ea83204a1d5
notarobot767 <12644755+notarobot767@users.noreply.github.com>
2022-09-17 22:02:10 -1000

    modified schema and changed default image

--- app/guac_db/schema/001-create-msql-user.sql
+++ app/guac_db/schema/001-create-msql-user.sql
@@ -1,4 +1,6 @@
 CREATE DATABASE guac;
+DROP USER 'dbadmin'@'%';
+flush privileges;
 CREATE USER 'dbadmin'@'%' identified by 'dbadmin';
 GRANT SELECT,INSERT,UPDATE,DELETE ON guac.* TO 'dbadmin'@'%';
 flush privileges;
