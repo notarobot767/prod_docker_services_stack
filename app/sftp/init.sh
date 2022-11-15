f62f4e42bc830dfa2f8330b58acb82d63dd514d4
notarobot767 <12644755+notarobot767@users.noreply.github.com>
2022-11-14 21:52:46 -1000

    cleaned up permissions to sftp user

--- app/sftp/init.sh
+++ app/sftp/init.sh
@@ -1,28 +1,46 @@
 #!/bin/bash
 
 if [ -z "$DEV_USER" ]
-#if var USER empty
+  #if var USER empty
 then
   echo "USER enviornment variable unset!"
 else
-  useradd -s /bin/bash $DEV_USER
-  chown $DEV_USER:$DEV_USER /app/sftp
-  echo ${DEV_USER}:${DEV_PW} | chpasswd
-  echo "HostKeyAlgorithms +ssh-rsa"               >> /etc/ssh/sshd_config && \
-  echo "PubkeyAcceptedKeyTypes +ssh-rsa"          >> /etc/ssh/sshd_config && \
-  echo "AllowUsers $DEV_USER"                     >> /etc/ssh/sshd_config
-  echo "Match User $DEV_USER"                     >> /etc/ssh/sshd_config
-  echo "ForceCommand internal-sftp -d /app/sftp"  >> /etc/ssh/sshd_config
-  echo "PasswordAuthentication yes"               >> /etc/ssh/sshd_config
-  echo "ChrootDirectory /app/"                    >> /etc/ssh/sshd_config
-  echo "Match User $DEV_USER"                     >> /etc/ssh/sshd_config
-  echo "PermitTunnel no"                          >> /etc/ssh/sshd_config
-  echo "AllowAgentForwarding no"                  >> /etc/ssh/sshd_config
-  echo "AllowTcpForwarding no"                    >> /etc/ssh/sshd_config
-  echo "X11Forwarding no"                         >> /etc/ssh/sshd_config
+  if id -u $DEV_USER &> /dev/null; then
+    echo "User exists, starting SSH immediately!"
+  else
+    #if user does not exist
+    echo "Creating user and running scipt!"
+    useradd -M -s /bin/false $DEV_USER
+      #-M create user without home folder
+      #-s specify shell to none
+    chown $DEV_USER:$DEV_USER /app/sftp
+      #ensure dir sftp is writable
+      #dir is created/mapped from service yml file
+    echo ${DEV_USER}:${DEV_PW} | chpasswd
+      #change passowrd from var in secrets
+    #echo "HostKeyAlgorithms +ssh-rsa"               >> /etc/ssh/sshd_config
+    #echo "PubkeyAcceptedKeyTypes +ssh-rsa"          >> /etc/ssh/sshd_config
+      #enable legacy support for cisco devices
+      #might not need
+    echo "AllowUsers $DEV_USER"                     >> /etc/ssh/sshd_config
+      #ensure DEV_USER is only user allowed for ssh
+    echo "Match User $DEV_USER"                     >> /etc/ssh/sshd_config
+      #next command only apply to DEV_USER
+    echo "ForceCommand internal-sftp"               >> /etc/ssh/sshd_config
+      #force use of SFTP. User cannot ssh
+    echo "PasswordAuthentication yes"               >> /etc/ssh/sshd_config
+    echo "ChrootDirectory /app/"                    >> /etc/ssh/sshd_config
+      #restrict root dir to /app
+      #app is owed by root, but /app/sftp is owned by DEV_USER
+      #DEV_USER can write in /app/sftp
+      #https://serverfault.com/questions/910789/chroot-sftp-possible-to-allow-user-to-write-to-current-chroot-directory
+    echo "PermitTunnel no"                          >> /etc/ssh/sshd_config
+    echo "AllowAgentForwarding no"                  >> /etc/ssh/sshd_config
+    echo "AllowTcpForwarding no"                    >> /etc/ssh/sshd_config
+    echo "X11Forwarding no"                         >> /etc/ssh/sshd_config
+      #turn off since not using
+  fi
 fi
-#https://serverfault.com/questions/910789/chroot-sftp-possible-to-allow-user-to-write-to-current-chroot-directory
-#https://www.cyberciti.biz/faq/how-to-use-sed-to-find-and-replace-text-in-files-in-linux-unix-shell/
 
 /usr/sbin/sshd -D
 #start ssh service in background
