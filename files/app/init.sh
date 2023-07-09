e2581e38ea818b34c4bdcb67265eb2237e61d095
notarobot767 <12644755+notarobot767@users.noreply.github.com>
2023-07-08 22:27:16 -1000

    sent error log to stdout

--- files/app/init.sh
+++ files/app/init.sh
@@ -5,8 +5,5 @@ ln -fs /usr/share/zoneinfo/$TZ /etc/localtime
 dpkg-reconfigure -f noninteractive tzdata
 #reconfigure timezone from TZ environment variable
 
-nginx -v
-#print nginx version, compiler version, and configure parameters 
-
 nginx "$@"
 #allows injection of tftp args from docker-compose.yml in command field
