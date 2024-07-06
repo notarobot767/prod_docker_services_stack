86c2e15e96393a1280cec2ba02c20938348d49f4
notarobot767 <12644755+notarobot767@users.noreply.github.com>
2024-07-05 15:15:34 -1000

    removed folders not in prod or test

--- files/app/init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "setting timezone from TZ environment variable..."
-ln -fs /usr/share/zoneinfo/$TZ /etc/localtime
-dpkg-reconfigure -f noninteractive tzdata
-#reconfigure timezone from TZ environment variable
-
-nginx "$@"
-#allows injection of tftp args from docker-compose.yml in command field
