d1a14ca7dad69d96b293227db789c27a6765107b
notarobot767 <12644755+notarobot767@users.noreply.github.com>
2024-09-21 21:32:45 -1000

    added files server to prod

--- prod_files/app/init.sh
+++ prod_files/app/init.sh
@@ -3,7 +3,7 @@
 echo "setting timezone from TZ environment variable..."
 ln -fs /usr/share/zoneinfo/$TZ /etc/localtime
 dpkg-reconfigure -f noninteractive tzdata
-#reconfigure timezone from TZ environment variable
+# reconfigure timezone from TZ environment variable
 
 nginx "$@"
-#allows injection of tftp args from docker-compose.yml in command field
+# allows injection of args from compose.yml in command field
