3bdaac83e27b91c2972070a5c8e07a5875d7dd71
notarobot767 <12644755+notarobot767@users.noreply.github.com>
2025-04-04 17:48:30 -1000

    added scripts

---
+++ _scripts/renew_cert.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd "$(dirname "$0")" || exit 1
+# ensure script will always set working dir to script location
+
+cd ../prod_letsencrypt
+docker compose pull
+docker compose up -d renew
+
+cd ../prod_proxy
+docker compose restart rproxy
---
+++ _scripts/update_containers.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+cd "$(dirname "$0")" || exit 1
+# ensure script will always set working dir to script location
+
+# update plex
+cd ../prod_plex
+docker compose pull; docker compose up -d
+
+# update vs-code
+cd ../prod_code
+docker compose pull; docker compose up -d
+
+# update bastion
+cd ../prod_bastion
+docker compose pull; docker compose up -d
+
+# update qbittorent
+cd ../prod_qbittorrent
+docker compose pull; docker compose up -d
+
+# update zabbix
+cd ../prod_zabbix
+docker compose pull; docker compose up -d
+
+# update authentik
+cd ../prod_authentik
+docker compose pull; docker compose up -d
